6fda98df7898ca604207704e9651dd969bebca51
EnzoPalmeira <143288625+EnzoPalmeira@users.noreply.github.com>
2025-07-09 00:16:08 -0300

    curso
    Sistema bancario dio

--- teste.py
+++
@@ -1,9 +0,0 @@
-numeros= [1,2,3,4,5,6,7,8,9,10]
-def filtrar_pares(a):
-   
-    for numero in a:
-        if numero % 2 == 0:
-            print(numero)
-            
-
-filtrar_pares(numeros)      
